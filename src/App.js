bf89fc2296ddaf6da1843de0dc484ed309fa66e1
dyahutami2017 <dyahutami.2017@student.uny.ac.id>
2019-07-15 11:46:55 +0700

    menambahkan background, menggeser posisi FormField dan button

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
 import React from 'react';
 import './App.css';
 import form from './view/form.jsx';
-import gambar from '../src/undraw_freelancer_b0my.png'
+
 
 
 function App() {
   return (
     <div className="App">
-      
+  
   <div className="App__Form">
-      
-                
+             
         <div className="PageSwitcher">
-        <div className="circle"></div> 
+        
         
         <div className="logo"></div>
         
-        </div><span className="judul">Fill the Form!!</span>
+        </div><span className="judul">Apa yang bisa saya bantu?</span>
  
  <form>   
     <div className="FormField">
       <label className="FormField__Label" htmlFor="name">Full Name</label>
       <input type="text" id="name" className="FormField__Input" placeholder="Nama Lengkap"/>
+      
     </div>
  
     <div className="FormField">
       <label className="FormField__Label" htmlFor="email">Email Address</label>
       <input type="text" id="email" className="FormField__Input" placeholder="Email"/>
+     
     </div>
-    <div className="FormField">
-      <img src={gambar} alt="undraw"/>
-      </div>
-    
+        
    <div className="FormField">
       <label className="FormField__Label" htmlFor="phone">Phone Number</label> 
       <input type="text" id="phone" className="FormField__Input" placeholder="No Telepon"/>
-      
     </div>
-    
-
  
     <div className="FormField">
       <label className="FormField__Label" htmlFor="message">Message</label>
@@ -50,9 +45,7 @@ function App() {
       </div>
       
   </form>
-          <div className="circle1">
-          
-          </div> 
+         
         </div>
         
     </div>
